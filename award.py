7ae9f32c1ed362a42b7ce314b31e04d23ae4844d
uzohc <148906938+uzohc@users.noreply.github.com>
2024-01-13 10:47:07 +0000

    Update award.py

--- award.py
+++ award.py
@@ -1,3 +1,7 @@
+#In this program, we want to issue 'Provincial colours' award for participants who complete the three sports in 100 minutes or less. 
+#We will issue the 'Provincial half colours'award to those who complete the sports in more than 100 minutes but less than or equal to 105 minutes.
+#We will issue the 'Provincial Scroll' award for those who complete the sports in 106 minutes but less than or equal to 110 minunites.
+#We will issue no award for a complete time above 110 minutes.
 swimming_time = int(input("Enter swimming time (minutes): ")) 
 cycling_time = int(input("Enter cycling time (minutes): "))
 running_time = int(input("Enter running time (minutes): "))
